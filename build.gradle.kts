c261241c7d9d9477383e2f50770562b42a3f1b06
Miko Väli <mikovali@gmail.com>
2024-08-07 14:04:44 +0300

    CI (#1)
    CI for merge and PR

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply  false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.google.hilt) apply false
@@ -12,3 +14,11 @@ plugins {
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.realm) apply false
 }
+
+val version = Properties().apply { load(file("version.properties").inputStream()) }
+val versionCode = version.getProperty("version.code")?.toInt() ?: error("version.code not found")
+val versionName = version.getProperty("version.name") ?: error("version.name not found")
+val isSnapshot = hasProperty("snapshot")
+
+extra["application.versionCode"] = versionCode
+extra["application.versionName"] = versionName + (if (isSnapshot) "-SNAPSHOT-$versionCode" else "")
