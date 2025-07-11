e23e21fe56c58b036916b9acb9ef127a1ea9f036
Miko Väli <miko.vali@fob-solutions.com>
2025-07-11 12:21:19 +0300

    Deps and CI badge

--- domain/src/main/kotlin/com/sensorfields/chore/android/domain/models/Chore.kt
+++ domain/src/main/kotlin/com/sensorfields/chore/android/domain/models/Chore.kt
@@ -8,11 +8,45 @@ public data class Chore(
     val id: Id,
     val name: String,
     val date: InstantSerializable?,
+    // val repeat: Repeat,
 ) {
-
     @JvmInline
     @Serializable
     public value class Id(public val value: String)
+
+//    @Serializable
+//    public sealed interface Repeat {
+//
+//        public val interval: Int
+//        public val time: LocalTime
+//
+//        @Serializable
+//        public data class Daily(
+//            public override val interval: Int,
+//            public override val time: LocalTime,
+//        ) : Repeat
+//
+//        @Serializable
+//        public data class Weekly(
+//            public override val interval: Int,
+//            public override val time: LocalTime,
+//            public val days: Set<DayOfWeek>,
+//        ) : Repeat
+//
+//        @Serializable
+//        public data class Monthly(
+//            public override val interval: Int,
+//            public override val time: LocalTime,
+//            public val days: Set<Int>,
+//        ) : Repeat
+//
+//        @Serializable
+//        public data class Yearly(
+//            public override val interval: Int,
+//            public override val time: LocalTime,
+//            public val days: Set<Pair<Month, Int>>,
+//        ) : Repeat
+//    }
 
     public enum class SortProperty { NAME, DATE }
 }
