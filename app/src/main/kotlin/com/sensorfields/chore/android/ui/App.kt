88ea71d80f7c3e364836b4e973c8f1532109f556
Miko Väli <miko.vali@fob-solutions.com>
2025-07-11 18:01:14 +0300

    Migrate to Navigation3 #20

--- app/src/main/kotlin/com/sensorfields/chore/android/ui/App.kt
+++ app/src/main/kotlin/com/sensorfields/chore/android/ui/App.kt
@@ -1,29 +1,57 @@
 package com.sensorfields.chore.android.ui
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.sensorfields.chore.android.ui.chore.create.choreCreate
-import com.sensorfields.chore.android.ui.chore.create.choreCreateResults
-import com.sensorfields.chore.android.ui.chore.create.navigateToChoreCreate
-import com.sensorfields.chore.android.ui.chore.details.choreDetails
-import com.sensorfields.chore.android.ui.chore.details.navigateToChoreDetails
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
+import androidx.navigation3.ui.NavDisplay
+import androidx.navigation3.ui.rememberSceneSetupNavEntryDecorator
+import com.sensorfields.chore.android.ui.Route.Home
 import com.sensorfields.chore.android.ui.home.HomeRoute
-import com.sensorfields.chore.android.ui.home.home
 import com.sensorfields.chore.android.ui.theme.AppTheme
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.serialization.Serializable
 
 @Composable
 fun App() {
     AppTheme {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = HomeRoute) {
-            home(
-                onNavigateToChoreCreate = navController::navigateToChoreCreate,
-                choreCreateResults = { navController.choreCreateResults },
-                onNavigateToChoreDetails = navController::navigateToChoreDetails
-            )
-            choreCreate(navController)
-            choreDetails(navController)
-        }
+        val backStack = rememberNavBackStack<Route>(Home)
+        NavDisplay(
+            backStack = backStack,
+            entryDecorators = listOf(
+                rememberSceneSetupNavEntryDecorator(),
+                rememberSavedStateNavEntryDecorator(),
+                rememberViewModelStoreNavEntryDecorator(),
+            ),
+            entryProvider = { key ->
+                when (key as Route) {
+                    Home -> NavEntry(key) {
+                        HomeRoute(
+                            onNavigateToChoreCreate = {},
+                            choreCreateResults = { emptyFlow() },
+                            onNavigateToChoreDetails = {},
+                        )
+                    }
+                }
+            }
+        )
+
+//        NavHost(navController = navController, startDestination = HomeRoute) {
+//            home(
+//                onNavigateToChoreCreate = navController::navigateToChoreCreate,
+//                choreCreateResults = { navController.choreCreateResults },
+//                onNavigateToChoreDetails = navController::navigateToChoreDetails
+//            )
+//            choreCreate(navController)
+//            choreDetails(navController)
+//        }
     }
+}
+
+sealed interface Route : NavKey {
+
+    @Serializable
+    data object Home : Route
 }
--- features/home/ui/src/main/kotlin/com/sensorfields/chore/android/ui/home/HomeRoute.kt
+++ features/home/ui/src/main/kotlin/com/sensorfields/chore/android/ui/home/HomeRoute.kt
@@ -1,38 +1,33 @@
 package com.sensorfields.chore.android.ui.home
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
-import androidx.navigation.compose.composable
 import androidx.navigation.navOptions
 import com.sensorfields.chore.android.domain.models.Chore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.serialization.Serializable
 
-@Serializable
-public object HomeRoute
-
-public fun NavGraphBuilder.home(
+@Composable
+public fun HomeRoute(
     onNavigateToChoreCreate: () -> Unit,
     choreCreateResults: () -> Flow<Chore>,
     onNavigateToChoreDetails: (Chore.Id) -> Unit,
 ) {
-    composable<HomeRoute> {
-        val viewModel = hiltViewModel<HomeViewModel>()
-        val state by viewModel.state.collectAsStateWithLifecycle()
-        HomeScreen(
-            state = state,
-            onScreenChange = viewModel::onScreenChange,
-            onNavigateToChoreCreate = onNavigateToChoreCreate,
-            choreCreateResults = choreCreateResults,
-            onNavigateToChoreDetails = onNavigateToChoreDetails
-        )
-    }
+    val viewModel = viewModel<HomeViewModel>()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    HomeScreen(
+        state = state,
+        onScreenChange = viewModel::onScreenChange,
+        onNavigateToChoreCreate = onNavigateToChoreCreate,
+        choreCreateResults = choreCreateResults,
+        onNavigateToChoreDetails = onNavigateToChoreDetails
+    )
 }
 
+@Deprecated("Replace with Nav3")
 internal val NavController.homeNavOptions: NavOptions
     get() = navOptions {
         popUpTo(graph.startDestinationId) {
