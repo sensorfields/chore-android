6675dbcd9bc82105a03e90e2b28e0e4418283c31
Miko Väli <miko.vali@fob-solutions.com>
2025-07-12 00:22:02 +0300

    Remove old navigation

--- features/chore/details/ui/src/main/kotlin/com/sensorfields/chore/android/ui/chore/details/ChoreDetailsRoute.kt
+++ features/chore/details/ui/src/main/kotlin/com/sensorfields/chore/android/ui/chore/details/ChoreDetailsRoute.kt
@@ -2,7 +2,7 @@ package com.sensorfields.chore.android.ui.chore.details
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sensorfields.chore.android.domain.models.Chore
 
--- features/dashboard/ui/src/main/kotlin/com/sensorfields/chore/android/ui/dashboard/DashboardRoute.kt
+++ features/dashboard/ui/src/main/kotlin/com/sensorfields/chore/android/ui/dashboard/DashboardRoute.kt
@@ -2,7 +2,7 @@ package com.sensorfields.chore.android.ui.dashboard
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sensorfields.chore.android.domain.models.Chore
 import com.sensorfields.chore.android.ui.collectInEffect
